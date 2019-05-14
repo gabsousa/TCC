585b3d16702f22c1280433826f52d73e3e4fd3eb
Caique Mota <caique.mota97@gmail.com>
2019-05-14 12:46:26 -0300

    Corrigindo a AgendaData

--- ProjetoTCC/Models/Data/AgendaData.cs
+++ ProjetoTCC/Models/Data/AgendaData.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ namespace ProjetoTCC.Models.Data
             MySqlConnection msc = new MySqlConnection("server=localhost; uid=root; pwd=123456789; database=bd_clinicare");
             msc.Open();
 
-            MySqlCommand cmd = new MySqlCommand("insert into tb_clientes values (@cod_agenda, @dia_consulta, @hora_consulta, @cod_medico, @cod_pac, @confirmacao_agenda, @obs_agenda, @resp_agendamento, @data_retorno)", msc);
+            MySqlCommand cmd = new MySqlCommand("insert into tb_clientes values (@cod_agenda, @dia_consulta, @hora_consulta, @cod_med, @cod_pac, @confirmacao_agenda, @obs_agenda, @resp_agendamento, @data_retorno)", msc);
 
             cmd.Parameters.AddWithValue("@cod_agenda", agenda.cod_agenda);
             cmd.Parameters.AddWithValue("@dia_consulta", agenda.dia_consulta);
@@ -29,7 +30,7 @@ namespace ProjetoTCC.Models.Data
             msc.Close();
         }
 
-        public List<Paciente> ListaPacient()
+        public List<Agenda> ListaAgenda()
         {
             MySqlConnection msc = new MySqlConnection("server=localhost; uid=root; pwd=123456789; database=bd_clinicare");
             msc.Open();
@@ -41,17 +42,21 @@ namespace ProjetoTCC.Models.Data
 
             msc.Close();
 
-            List<Paciente> lista = new List<Paciente>();
+            List<Agenda> lista = new List<Agenda>();
 
             foreach (DataRow dr in ds.Tables[0].Rows)
             {
-                Paciente item = new Paciente();
+                Agenda item = new Agenda();
 
+                item.cod_agenda = int.Parse(dr["cod_agenda"].ToString());
+                item.dia_consulta = DateTime.Parse(dr["dia_consulta"].ToString());
+                item.hora_consulta = DateTime.Parse(dr["hora_consulta"].ToString());
+                item.cod_med = int.Parse(dr["cod_med"].ToString());
                 item.cod_pac = int.Parse(dr["cod_pac"].ToString());
-                item.nome_pac = dr["nome_pac"].ToString();
-                item.datanasc_pac = DateTime.Parse(dr["datanasc_pac"].ToString());
-                item.sexo_pac = dr["sexo_pac"].ToString();
-                item.idade_pac = int.Parse(dr["idade_pac"].ToString());
+                item.confirmacao_agenda = dr["confirmacao_agenda"].ToString();
+                item.obs_agenda = dr["obs_agenda"].ToString();
+                item.resp_agendamento = dr["resp_agendamento"].ToString();
+                item.data_retorno = DateTime.Parse(dr["data_retorno"].ToString());
 
                 lista.Add(item);
             }
@@ -59,7 +64,7 @@ namespace ProjetoTCC.Models.Data
             return lista;
         }
 
-        public Paciente DetalhesPaciente(int cod_pac)
+        public Agenda DetalhesAgenda(int cod_pac)
         {
             MySqlConnection msc = new MySqlConnection("server=localhost; uid=root; pwd=123456789; database=bd_clinicare");
             msc.Open();
@@ -71,40 +76,56 @@ namespace ProjetoTCC.Models.Data
 
             msc.Close();
 
-            List<Paciente> lista = new List<Paciente>();
+            List<Agenda> lista = new List<Agenda>();
 
-            Paciente item = new Paciente();
+            Agenda item = new Agenda();
 
             if (ds.Tables[0].Rows.Count > 0)
             {
+                item.cod_agenda = int.Parse(ds.Tables[0].Rows[0]["cod_agenda"].ToString());
+                item.dia_consulta = DateTime.Parse(ds.Tables[0].Rows[0]["dia_consulta"].ToString());
+                item.hora_consulta = DateTime.Parse(ds.Tables[0].Rows[0]["hora_consulta"].ToString());
+                item.cod_med = int.Parse(ds.Tables[0].Rows[0]["cod_med"].ToString());
                 item.cod_pac = int.Parse(ds.Tables[0].Rows[0]["cod_pac"].ToString());
-                item.nome_pac = ds.Tables[0].Rows[0]["nome_pac"].ToString();
-                item.datanasc_pac = DateTime.Parse(ds.Tables[0].Rows[0]["datanasc_pac"].ToString());
-                item.sexo_pac = ds.Tables[0].Rows[0]["sexo_pac"].ToString();
-                item.idade_pac = int.Parse(ds.Tables[0].Rows[0]["idade_pac"].ToString());
+                item.confirmacao_agenda = ds.Tables[0].Rows[0]["confirmacao_agenda"].ToString();
+                item.obs_agenda = ds.Tables[0].Rows[0]["obs_agenda"].ToString();
+                item.resp_agendamento = ds.Tables[0].Rows[0]["resp_agendamento"].ToString();
+                item.data_retorno = DateTime.Parse(ds.Tables[0].Rows[0]["data_retorno"].ToString());
             }
 
             return item;
         }
 
-        public void EditarPaciente(Paciente paciente)
+        public void EditarAgenda(Agenda agenda)
         {
             MySqlConnection msc = new MySqlConnection("server=localhost; uid=root; pwd=123456789; database=bd_clinicare");
             msc.Open();
 
-            MySqlCommand cmd = new MySqlCommand("insert into tb_clientes values (@cod_agenda, @dia_consulta, @hora_consulta, @cod_medico(FK), @cod_pac(FK), @confirmacao_agenda, @obs_agenda, @resp_agendamento, @data_retorno)", msc);
+            MySqlCommand cmd = new MySqlCommand("update tb_clientes set cod_pac = @cod_pac, nome_pac = @nome_pac, datanasc_pac = @datanasc_pac, sexo_pac = @sexo_pac, idade_pac = @idade_pac where cod_pac = @cod_pac ", msc);
 
-            cmd.Parameters.AddWithValue("@cod_agenda", Agenda.cod_agenda);
-            cmd.Parameters.AddWithValue("@dia_consulta", Agenda.dia_consulta);
-            cmd.Parameters.AddWithValue("@hora_consulta", Agenda.hora_consultahora_consulta);
-            cmd.Parameters.AddWithValue("@cod_medico", Agenda.cod_medico);
-            cmd.Parameters.AddWithValue("@cod_pac", Agenda.cod_pac);
-            cmd.Parameters.AddWithValue("@confirmacao_agenda", Agenda.confirmacao_agenda);
-            cmd.Parameters.AddWithValue("@obs_agenda", Agenda.obs_agenda);
-            cmd.Parameters.AddWithValue("@resp_agendamento", Agenda.resp_agendamento);
-            cmd.Parameters.AddWithValue("@data_retorno", Agenda.data_retorno);
+            cmd.Parameters.AddWithValue("@cod_agenda", agenda.cod_agenda);
+            cmd.Parameters.AddWithValue("@dia_consulta", agenda.dia_consulta);
+            cmd.Parameters.AddWithValue("@hora_consulta", agenda.hora_consulta);
+            cmd.Parameters.AddWithValue("@cod_med", agenda.cod_med);
+            cmd.Parameters.AddWithValue("@cod_pac", agenda.cod_pac);
+            cmd.Parameters.AddWithValue("@confirmacao_agenda", agenda.confirmacao_agenda);
+            cmd.Parameters.AddWithValue("@obs_agenda", agenda.obs_agenda);
+            cmd.Parameters.AddWithValue("@resp_agendamento", agenda.resp_agendamento);
+            cmd.Parameters.AddWithValue("@data_retorno", agenda.data_retorno);
 
             cmd.ExecuteNonQuery();
+            msc.Close();
+        }
+
+        public void DeletarAgenda(int cod_pac)
+        {
+            MySqlConnection msc = new MySqlConnection("server=localhost; uid=root; pwd=123456789; database=bd_clinicare");
+            msc.Open();
+
+            MySqlCommand command = new MySqlCommand("delete from tb_clientes where cod_agenda = " + cod_pac, msc);
+
+            command.ExecuteNonQuery();
+
             msc.Close();
         }
     }
