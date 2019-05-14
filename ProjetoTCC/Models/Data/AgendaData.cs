512b86da12e1c04e8ef5ba2f7b6090253dc74cac
Gabriela <sousagaby90@yahoo.com.br>
2019-05-13 21:50:45 -0300

    Correção de erros

--- ProjetoTCC/Models/Data/AgendaData.cs
+++ ProjetoTCC/Models/Data/AgendaData.cs
@@ -12,17 +12,17 @@ namespace ProjetoTCC.Models.Data
             MySqlConnection msc = new MySqlConnection("server=localhost; uid=root; pwd=123456789; database=bd_clinicare");
             msc.Open();
 
-            MySqlCommand cmd = new MySqlCommand("insert into tb_clientes values (@cod_agenda, @dia_consulta, @hora_consulta, @cod_medico(FK), @cod_pac(FK), @confirmacao_agenda, @obs_agenda, @resp_agendamento, @data_retorno)", msc);
+            MySqlCommand cmd = new MySqlCommand("insert into tb_clientes values (@cod_agenda, @dia_consulta, @hora_consulta, @cod_med, @cod_pac, @confirmacao_agenda, @obs_agenda, @resp_agendamento, @data_retorno)", msc);
 
-            cmd.Parameters.AddWithValue("@cod_agenda", Agenda.cod_agenda);
-            cmd.Parameters.AddWithValue("@dia_consulta", Agenda.dia_consulta);
-            cmd.Parameters.AddWithValue("@hora_consulta", Agenda.hora_consultahora_consulta);
-            cmd.Parameters.AddWithValue("@cod_medico", Agenda.cod_medico);
-            cmd.Parameters.AddWithValue("@cod_pac", Agenda.cod_pac);
-            cmd.Parameters.AddWithValue("@confirmacao_agenda", Agenda.confirmacao_agenda);
-            cmd.Parameters.AddWithValue("@obs_agenda", Agenda.obs_agenda);
-            cmd.Parameters.AddWithValue("@resp_agendamento", Agenda.resp_agendamento);
-            cmd.Parameters.AddWithValue("@data_retorno", Agenda.data_retorno);
+            cmd.Parameters.AddWithValue("@cod_agenda", agenda.cod_agenda);
+            cmd.Parameters.AddWithValue("@dia_consulta", agenda.dia_consulta);
+            cmd.Parameters.AddWithValue("@hora_consulta", agenda.hora_consulta);
+            cmd.Parameters.AddWithValue("@cod_med", agenda.cod_med);
+            cmd.Parameters.AddWithValue("@cod_pac", agenda.cod_pac);
+            cmd.Parameters.AddWithValue("@confirmacao_agenda", agenda.confirmacao_agenda);
+            cmd.Parameters.AddWithValue("@obs_agenda", agenda.obs_agenda);
+            cmd.Parameters.AddWithValue("@resp_agendamento", agenda.resp_agendamento);
+            cmd.Parameters.AddWithValue("@data_retorno", agenda.data_retorno);
 
             cmd.ExecuteNonQuery();
             msc.Close();
