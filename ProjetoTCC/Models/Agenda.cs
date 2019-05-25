6ae1eac59e138e4db7b55a04304374398b029a2e
Caique Mota <caique.mota97@gmail.com>
2019-05-24 21:06:54 -0300

    Correção dos nomes das colunas.

--- ProjetoTCC/Models/Agenda.cs
+++ ProjetoTCC/Models/Agenda.cs
@@ -9,30 +9,30 @@ namespace ProjetoTCC.Models
     public class Agenda
     {
         [DisplayName("Código de Agendamento")]
-        public int cod_agenda { get; set; }
+        public int COD_AGENDA { get; set; }
 
         [DisplayName("Dia da Consulta")]
-        public DateTime dia_consulta { get; set; }
+        public DateTime DIA_CONSULTA { get; set; }
 
         [DisplayName("Hora da Consulta")]
-        public DateTime hora_consulta { get; set; }
+        public DateTime HORA_CONSULTA { get; set; }
 
         [DisplayName("Código do Médico")]
-        public int cod_med { get; set; }
+        public int COD_MED { get; set; }
      
         [DisplayName("Código do Paciente")]
-        public int cod_pac { get; set; }
+        public int COD_PAC { get; set; }
 
         [DisplayName("Confirmação da Agenda")]
-        public string confirmacao_agenda { get; set; }
+        public string CONFIRMACAO_AGENDA { get; set; }
 
         [DisplayName("Observação da Agenda")]
-        public string obs_agenda { get; set; }
+        public string OBS_AGENDA { get; set; }
 
         [DisplayName("Responsável Pelo Agendamento")]
-        public string resp_agendamento { get; set; }
+        public string RESP_AGENDAMENTO { get; set; }
 
         [DisplayName("Data de Retorno da Cosulta")]
-        public DateTime data_retorno { get; set; }
+        public DateTime DATA_RETORNO { get; set; }
     }
 }
