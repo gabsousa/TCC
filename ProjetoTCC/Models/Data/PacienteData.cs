da9494eb2bc98a6c83b84a1c1068c58af1527a29
Caique Mota <caique.mota97@gmail.com>
2019-06-20 21:20:55 -0300

    Alterações Paciente.

--- ProjetoTCC/Models/Data/PacienteData.cs
+++ ProjetoTCC/Models/Data/PacienteData.cs
@@ -35,7 +35,6 @@ namespace ProjetoTCC.Models.Data
             cmd.Parameters.AddWithValue("@NUM_PAC", paciente.NUM_PAC);
             cmd.Parameters.AddWithValue("@CEP_PAC", paciente.CEP_PAC);
             cmd.Parameters.AddWithValue("@BAIRRO_PAC", paciente.BAIRRO_PAC);
-            cmd.Parameters.AddWithValue("@COD_LOGIN", paciente.COD_LOGIN);
             cmd.ExecuteNonQuery();
 
             msc.Close();
@@ -70,13 +69,13 @@ namespace ProjetoTCC.Models.Data
                 item.TEL_RESP = int.Parse(dr["TEL_RESP"].ToString());
                 item.TEL_DOIS = int.Parse(dr["TEL_DOIS"].ToString());
                 item.CEL_RESP = int.Parse(dr["CEL_RESP"].ToString());
-                item.CPF_RESP = int.Parse(dr["CPF_RESP"].ToString());
+                item.CPF_RESP = dr["CPF_RESP"].ToString();
                 item.RUA_PAC = dr["RUA_PAC"].ToString();
-                item.MUN_PAC = dr["MUN_PAC"].ToString();
-                item.UF_PAC = dr["UF_PAC"].ToString();
                 item.NUM_PAC = int.Parse(dr["NUM_PAC"].ToString());
                 item.CEP_PAC = int.Parse(dr["CEP_PAC"].ToString());
                 item.BAIRRO_PAC = dr["BAIRRO_PAC"].ToString();
+                item.MUN_PAC = dr["MUN_PAC"].ToString();
+                item.UF_PAC = dr["UF_PAC"].ToString();
                 lista.Add(item);
             }
 
@@ -104,20 +103,21 @@ namespace ProjetoTCC.Models.Data
                 item.COD_PAC = int.Parse(ds.Tables[0].Rows[0]["COD_PAC"].ToString());
                 item.NOME_PAC = ds.Tables[0].Rows[0]["NOME_PAC"].ToString();
                 item.DATANASC_PAC = DateTime.Parse(ds.Tables[0].Rows[0]["DATANASC_PAC"].ToString());
-                item.SEXO_PAC = ds.Tables[0].Rows[0]["SEXO_PAC"].ToString();
                 item.IDADE_PAC = int.Parse(ds.Tables[0].Rows[0]["IDADE_PAC"].ToString());
+                item.SEXO_PAC = ds.Tables[0].Rows[0]["SEXO_PAC"].ToString();
                 item.NOME_RESP = ds.Tables[0].Rows[0]["NOME_RESP"].ToString();
-                item.RESP_DOIS = ds.Tables[0].Rows[0]["RESP_DOIS"].ToString();
                 item.TEL_RESP = int.Parse(ds.Tables[0].Rows[0]["TEL_RESP"].ToString());
-                item.TEL_DOIS = int.Parse(ds.Tables[0].Rows[0]["TEL_DOIS"].ToString());
                 item.CEL_RESP = int.Parse(ds.Tables[0].Rows[0]["CEL_RESP"].ToString());
-                item.CPF_RESP = int.Parse(ds.Tables[0].Rows[0]["CPF_RESP"].ToString());
+                item.RESP_DOIS = ds.Tables[0].Rows[0]["RESP_DOIS"].ToString();
+                item.TEL_DOIS = int.Parse(ds.Tables[0].Rows[0]["TEL_DOIS"].ToString());
+                item.CPF_RESP = ds.Tables[0].Rows[0]["CPF_RESP"].ToString();
+                item.EMAIL_RESP = ds.Tables[0].Rows[0]["EMAIL_RESP"].ToString();
                 item.RUA_PAC = ds.Tables[0].Rows[0]["RUA_PAC"].ToString();
-                item.MUN_PAC = ds.Tables[0].Rows[0]["MUN_PAC"].ToString();
-                item.UF_PAC = ds.Tables[0].Rows[0]["UF_PAC"].ToString();
                 item.NUM_PAC = int.Parse(ds.Tables[0].Rows[0]["NUM_PAC"].ToString());
                 item.CEP_PAC = int.Parse(ds.Tables[0].Rows[0]["CEP_PAC"].ToString());
                 item.BAIRRO_PAC = ds.Tables[0].Rows[0]["BAIRRO_PAC"].ToString();
+                item.MUN_PAC = ds.Tables[0].Rows[0]["MUN_PAC"].ToString();
+                item.UF_PAC = ds.Tables[0].Rows[0]["UF_PAC"].ToString();
             }
 
             return item;
@@ -137,19 +137,18 @@ namespace ProjetoTCC.Models.Data
             cmd.Parameters.AddWithValue("@SEXO_PAC", paciente.SEXO_PAC);
             cmd.Parameters.AddWithValue("@IDADE_PAC", paciente.IDADE_PAC);
             cmd.Parameters.AddWithValue("@NOME_RESP", paciente.NOME_RESP);
-            cmd.Parameters.AddWithValue("@RESP_DOIS", paciente.RESP_DOIS);
             cmd.Parameters.AddWithValue("@TEL_RESP", paciente.TEL_RESP);
-            cmd.Parameters.AddWithValue("@TEL_DOIS", paciente.TEL_DOIS);
             cmd.Parameters.AddWithValue("@CEL_RESP", paciente.CEL_RESP);
+            cmd.Parameters.AddWithValue("@RESP_DOIS", paciente.RESP_DOIS);
+            cmd.Parameters.AddWithValue("@TEL_DOIS", paciente.TEL_DOIS);
             cmd.Parameters.AddWithValue("@EMAIL_RESP", paciente.EMAIL_RESP);
             cmd.Parameters.AddWithValue("@CPF_RESP", paciente.CPF_RESP);
             cmd.Parameters.AddWithValue("@RUA_PAC", paciente.RUA_PAC);
-            cmd.Parameters.AddWithValue("@MUN_PAC", paciente.MUN_PAC);
-            cmd.Parameters.AddWithValue("@UF_PAC", paciente.UF_PAC);
             cmd.Parameters.AddWithValue("@NUM_PAC", paciente.NUM_PAC);
             cmd.Parameters.AddWithValue("@CEP_PAC", paciente.CEP_PAC);
             cmd.Parameters.AddWithValue("@BAIRRO_PAC", paciente.BAIRRO_PAC);
-            cmd.Parameters.AddWithValue("@COD_LOGIN", paciente.COD_LOGIN);
+            cmd.Parameters.AddWithValue("@MUN_PAC", paciente.MUN_PAC);
+            cmd.Parameters.AddWithValue("@UF_PAC", paciente.UF_PAC);
             cmd.ExecuteNonQuery();
 
             msc.Close();
